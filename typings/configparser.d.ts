f1bb0d2d650a9b6b01575aaf7ab3719ce020efb3
Yslan Ramos <yslan3000@gmail.com>
2020-12-13 18:48:51 -0300

    add typings

---
+++ typings/configparser.d.ts
@@ -0,0 +1,22 @@
+type FileInputType = string | Buffer | number;
+
+declare class ConfigParser {
+  sections(): string[];
+  addSection(section: string): void;
+  hasSection(section: string): boolean;
+  keys(section: string): string[];
+  hasKey(section: string, key: string): boolean;
+  read(file: FileInputType): void;
+  readAsync(file: FileInputType): void;
+  get(section: string, key: string, raw?: boolean): string | undefined;
+  getInt(section: string, key: string, radix?: number): number | undefined;
+  getFloat(section: string, key: string): number|undefined;
+  items(section: string): Record<string, string>;
+  set(section: string, key: string, value: any): void;
+  removeKey(section: string, key: string): boolean;
+  removeSection(section: string): boolean;
+  write(file: FileInputType, createMissingDirs?: boolean): void;
+  writeSync(file: FileInputType, createMissingDirs?: boolean): Promise<void>;
+}
+
+export default ConfigParser
